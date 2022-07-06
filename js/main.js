bbf447145da3c79e465bf4aea9bada644c06eeed
Evgeniy <50502341+Facebaker@users.noreply.github.com>
2022-07-07 01:57:17 +0300

    Удалил часть кода

--- js/main.js
+++ js/main.js
@@ -2,16 +2,7 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function getLengthComment(line, length) {
-  if (line.length <= 0 || line.length > length) {
-    return 'false';
-  } else {
-    return 'true';
-  }
-  getLengthComment(5, 12);
-
-
-  function maxLength(str, length) {
-    return str.length > length;
-  }
-  maxLength('vrfve', 10)
+function maxLength(str, length) {
+  return str.length > length;
+}
+maxLength('return true', 10);
